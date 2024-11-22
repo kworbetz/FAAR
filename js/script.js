1f83a508b4d12ec67b57fc029c381ed82b9e20c0
Kevyn <kworbetz@gmail.com>
2024-11-22 16:45:22 -0700

    add more art and fix size

--- js/script.js
+++ js/script.js
@@ -2,10 +2,6 @@
 let debug = true;
 
 if (debug) {
-  console.log("Script loaded");
-}
-
-if (debug) {
   document.addEventListener("DOMContentLoaded", () => {
     const latElement = document.createElement("div");
     const lonElement = document.createElement("div");
@@ -69,29 +65,15 @@ if (debug) {
 }
 
 window.onload = () => {
-  let testEntityAdded = false;
-  console.log("GPS Debug script loaded");
-  const el = document.querySelector("[gps-new-camera]");
-
-  el.addEventListener("gps-camera-update-position", (e) => {
-    if (!testEntityAdded) {
-      console.warn(
-        `Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`
-      );
-      // Add a box to the north of the initial GPS position
-      const entity = document.createElement("a-box");
-      entity.setAttribute("scale", {
-        x: 20,
-        y: 20,
-        z: 20,
+  AFRAME.registerComponent("cursor-listener", {
+    init: function () {
+      var lastIndex = -1;
+      var COLORS = ["red", "green", "blue"];
+      this.el.addEventListener("click", function (evt) {
+        lastIndex = (lastIndex + 1) % COLORS.length;
+        this.setAttribute("material", "color", COLORS[lastIndex]);
+        console.log("I was clicked at: ", evt.detail.intersection.point);
       });
-      entity.setAttribute("material", { color: "red" });
-      entity.setAttribute("gps-new-entity-place", {
-        latitude: e.detail.position.latitude,
-        longitude: e.detail.position.longitude + 0.0001,
-      });
-      document.querySelector("a-scene").appendChild(entity);
-    }
-    testEntityAdded = true;
+    },
   });
 };
